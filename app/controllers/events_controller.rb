000eb5c296c8e7673ca378bf34e5be94cdff6d42
Dylan Crawshaw <dcrawsh@gmail.com>
2020-09-28 22:11:49 -0700

    Update events_controller.rb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,24 @@
 class EventsController < ApplicationController
+    
     def index
         @events = Event.order('start_datetime ASC')
     end
+
+    def create
+        @event = Event.new(event_params)
+        if @event.save
+            render json: @event 
+        else 
+            render json: @event.erros, status: :unprocessible_entity
+        end
+    end
+
+    private
+    
+    def event_params
+        params.require(:event).permit(:title, :start_datetime, :location)
+    end
+
+
 
 end
